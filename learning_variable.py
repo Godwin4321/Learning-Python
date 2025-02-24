1650b83faeefaaf5bd80265ce41f3eb7f03d604c
Godwin4321 <godwinjayakumar1@gmail.com>
2025-02-24 21:25:52 +0530

    Learning variables

---
+++ learning_variable.py
@@ -0,0 +1,12 @@
+price = 10
+#price = 20  # integer --> number without decimal point
+rating = 4.9 #float --> number with decimal point
+name = "Godwin"  #string
+is_published = True  #boolean
+
+new_name = "John Smith"
+age = 20
+new_patient = True
+
+
+print(price)
